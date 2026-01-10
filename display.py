fabaa0f5ea5adf81e1a5cba1df7790d43ebda72e
windbell0711 <windbell07@outlook.com>
2026-01-10 19:25:44 +0800

    ljq upd: thread

--- display.py
+++ display.py
@@ -1,9 +1,13 @@
+import time
 import pygame
 import sys
-import game as gm
 
-## TODO: TEMP!!!
-n2c = {1:'车',2:'马',3:'相',4:'士',5:'炮',6:'帅',7:'卒',8:'城',9:'教',10:'骑',11:'后',12:'王',13:'兵'}
+import game as gm
+import ctypes
+
+## TEMP!!!
+n2c = {1:'车',2:'马',3:'相',4:'士',5:'炮',6:'帅',7:'卒',8:'城',9:'教',10:'骑',11:'后',12:'王',13:'兵',14:'#'}
+FONT = '华文隶书'
 
 def get_grid_position(x, y, square_x, square_y, square_size, grid_size=11):
     """计算鼠标点击在网格中的位置"""
@@ -32,15 +36,20 @@ def poses2beach_p(mouse_x, mouse_y, square_x, square_y, square_size):
     else:
         return None
 
+if sys.platform == 'win32':
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+
 def play():
     pygame.init()
     game = gm.Game()
-    width, height = 800, 600
+    width, height = 1300, 1000
     square_size = min(width, height)
     square_x = (width - square_size) // 2
     square_y = (height - square_size) // 2
     cell_size = square_size / 11
-    font = pygame.font.SysFont('STLiti', int(cell_size))
+    line_w = int(cell_size / 20)
+    font = pygame.font.SysFont(FONT, int(cell_size))
+
     #此处应包含贴图初始化
 
     screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
@@ -61,7 +70,8 @@ def play():
                 square_x = (width - square_size) // 2
                 square_y = (height - square_size) // 2
                 cell_size = square_size / 11
-                font = pygame.font.SysFont('STLiti', int(cell_size))
+                line_w = int(cell_size / 20)
+                font = pygame.font.SysFont(FONT, int(cell_size))
                 # 此处应包含贴图大小调整
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
@@ -71,7 +81,12 @@ def play():
                     square_x = (width - square_size) // 2
                     square_y = (height - square_size) // 2
                     number = poses2beach_p(mouse_x, mouse_y, square_x, square_y, square_size)
-        flipped, steady, animations, last_choice, paths, UIs = game.handle_input_p(number)
+
+        st = time.time()
+        flipped, steady, animations, last_choice, paths, UIs, pressed = game.handle_input_p(number)
+        if time.time()-st>0.01:
+            print(time.time()-st)
+            print(flipped, steady, animations, last_choice, paths, UIs, pressed)
 
         #####以下可以删++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -82,27 +97,28 @@ def play():
         for i in range(12):
             pygame.draw.line(red_square, (150, 150, 150),
                              (i * cell_size, 0),
-                             (i * cell_size, square_size), 1)
+                             (i * cell_size, square_size), line_w)
             pygame.draw.line(red_square, (150, 150, 150),
                              (0, i * cell_size),
-                             (square_size, i * cell_size), 1)
+                             (square_size, i * cell_size), line_w)
         screen.blit(red_square, (square_x, square_y))
 
+        #静态棋子
         for p, typ in steady:
             #!
             if flipped:
                 p = 80-p
             text_surface = font.render(n2c[typ], True, (200, 200, 200) if typ > 7 else (220, 100, 100))
             screen.blit(text_surface,(square_x + p%9 * cell_size + cell_size,square_y + p//9 * cell_size+ cell_size))
-
+        #高亮棋子
         if last_choice[0] >= 0:
             p, typ = last_choice
             if flipped:
                 p = 80-p
             #!
-            text_surface = font.render(n2c[typ], True, (255, 255, 255) if typ > 7 else (250, 100, 100))
+            text_surface = font.render(n2c[typ], True, (255, 255, 255) if typ > 7 else (250, 150, 150))
             screen.blit(text_surface,(square_x + p % 9 * cell_size + cell_size, square_y + p // 9 * cell_size + int(cell_size*0.9)))
-
+        #棋子动画
         for fp, tp, jd, du, typ in animations:
             if jd < 0:
                 continue
@@ -111,17 +127,17 @@ def play():
             #!
             text_surface = font.render(n2c[typ], True, (200, 200, 200) if typ > 7 else (220, 100, 100))
             rat = jd/du
-            tx = int((rat * (tp%9-fp%9)+fp%9) * cell_size)
-            ty = int((rat * (tp // 9 - fp // 9) + fp // 9) * cell_size)
+            tx = (rat * (tp%9-fp%9)+fp%9) * cell_size
+            ty = (rat * (tp // 9 - fp // 9) + fp // 9) * cell_size
             screen.blit(text_surface,(square_x + tx + cell_size,square_y + ty+ cell_size))
-
+        #路径
         for p in paths:
             if flipped:
                 p = 80-p
             #!
             text_surface = font.render('·', True, (200, 200, 200))
             screen.blit(text_surface,(square_x + p % 9 * cell_size + cell_size, square_y + p // 9 * cell_size + cell_size))
-
+        #UI
         for p, rule in UIs:
             #!
             text_surface = font.render(rule, True, (200, 200, 200))
@@ -129,6 +145,33 @@ def play():
                 screen.blit(text_surface,(square_x,square_y + p%10 * cell_size))
             elif p//10 == 9:
                 screen.blit(text_surface,(square_x + cell_size*10,square_y + p%10 * cell_size))
+        #UI按下的效果
+        if pressed[-1] > 0:
+            p = pressed[0]
+            #!
+            text_surface = font.render('|', True, (200, 200, 200))
+            if p // 10 == 8:
+                screen.blit(text_surface, (square_x, square_y + p % 10 * cell_size))
+            elif p // 10 == 9:
+                screen.blit(text_surface, (square_x + cell_size * 10, square_y + p % 10 * cell_size))
+
+        # if True:
+        #     setting_surface = pygame.Surface((width, height), pygame.SRCALPHA)
+        #     setting_surface.fill((0,0,0,150))
+        #     pygame.draw.line(setting_surface, (220, 220, 220),
+        #                      (5*cell_size, 0),
+        #                      (5*cell_size, height), line_w*2)
+        
+        #     head_menu = ['选项设置', '选项', '另一个选', '就这样']
+        #     n=-0.5
+        #     for title in head_menu:
+        #         #!
+        #         n+=1
+        #         text_surface = font.render(title, True, (200, 200, 200))
+        #         setting_surface.blit(text_surface,(cell_size/2, cell_size*n))
+        
+        
+        #     screen.blit(setting_surface,(0,0))
 
         ##+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         pygame.display.flip()
--- game.py
+++ game.py
@@ -11,39 +11,63 @@ class Game:
         self.last_choice_piece = (-1,-1)
         self.UIs = set()
         self.board_is_flipped = False
+        self.pressed_button = [-1,0]
         #走子记录相关
         self.state = 'play'
         self.moves = []
         self.move_step = 0
+        self.promotion_move = ''
         # 游戏规则相关
         self.ai_chn = False
         self.ai_int = False
+        self.ai_think_time = 500
 
-    
     def reset_special_pieces_show(self):
         self.highlight_paths = []
         self.last_choice_piece = (-1,-1)
 
     def handle_input_p(self, display_p):
         # 下棋
-        if display_p is not None:
-            if self.state == 'play':
+        if self.state == 'play':
+            if display_p is not None:
                 if 0 <= display_p <= 80:
-                    ## 待办：这里要适配翻转棋盘 ##
                     self.handle_board_event(display_p if not self.board_is_flipped else 80 - display_p)
                 elif 81 <= display_p:
                     self.reset_special_pieces_show()
                     self.handle_game_UIs(display_p)
                     self.adjust_ai(display_p)
-            elif self.state == 'wait':
-                self.adjust_ai(display_p)
-        if self.state == 'play' or self.state == 'wait':
             self.process_animation()
             self.process_steady_pieces()
             self.process_UIs()
+        elif self.state == 'wait' or self.state == 'waits':
+            if display_p is not None:
+                self.adjust_ai(display_p)
+            self.process_animation()
+            self.process_steady_pieces()
+            self.process_UIs()
+        elif self.state == 'promotion':
+            if display_p is not None:
+                if self.board_is_flipped:
+                    self.handle_promotion_select(80-display_p)
+                else:
+                    self.handle_promotion_select(display_p)
         # 返回全部显示组件
         return (self.board_is_flipped, self.steady_pieces, self. piece_animations,
-                self.last_choice_piece, self.highlight_paths, self.UIs)
+                self.last_choice_piece, self.highlight_paths, self.UIs, self.pressed_button)
+
+    def handle_promotion_select(self, p):
+        tp = beach.fsf2beach(self.promotion_move[2:4])
+        z = None
+        if p == tp:
+            z = 'q'
+        elif p == tp -9:
+            z = 'n'
+        elif p == tp -18:
+            z = 'b'
+        elif p == tp - 27:
+            z = 'r'
+        if z is not None:
+            self.apply_move(self.promotion_move+z)
 
     def adjust_ai(self, p):
         if self.board_is_flipped:
@@ -60,18 +84,24 @@ class Game:
     def handle_game_UIs(self, p):
         if p == 98:
             if self.ai_chn ^ self.ai_int:
-                self.undo();self.undo()
+                self.thread_undo(2)
             else:
-                self.undo()
+                self.thread_undo()
+            self.pressed_button = [98, 10]
         elif p == 99:
-            self.gret()
+            self.thread_gret()
+            self.pressed_button = [99, 10]
         elif p == 96:
             self.board_is_flipped ^= 1
+            self.pressed_button = [96, 10]
         elif p == 91:
             self.auto_move()
 
     # 删除结束的动画，进度 +1
     def process_animation(self):
+        if len(self.piece_animations) > 20:
+            self.piece_animations.clear()
+            return
         temp = []
         for ani in self.piece_animations:
             if not ani[2] >= ani[3]:
@@ -87,52 +117,79 @@ class Game:
         p = 0
         for typ in self.board:
             if typ > 0 and p not in pieces_to_hide:
-                ## 待办：这里要适配翻转棋盘 ##
                 self.steady_pieces.add((p, typ))
             p += 1
 
+    #这个顺便处理UI按钮点击效果
     def process_UIs(self):
         self.UIs.clear()
-        if self.state == 'play':
+        if self.state == 'play' or self.state == 'waits':
             self.UIs.add((91, '!'))
+            self.UIs.add((96, '&'))
             self.UIs.add((98, '<'))
             self.UIs.add((99, '>'))
-            self.UIs.add((96, '&'))
-        if self.state == 'play' or self.state == 'wait':
+        if self.state == 'play' or self.state == 'wait' or self.state == 'waits':
             if self.board_is_flipped:
                 self.UIs.add((89, 'r' if self.ai_int else 'h'))
                 self.UIs.add((81, 'r' if self.ai_chn else 'h'))
             else:
                 self.UIs.add((81, 'r' if self.ai_int else 'h'))
                 self.UIs.add((89, 'r' if self.ai_chn else 'h'))
+        if self.pressed_button[-1] > 0:
+            self.pressed_button[-1] -= 1
 
-    def _am(self):
-        move = self.board.get_best_move(think_time=1000)
-        self.apply_move(move)
-
-    def auto_move(self):
-        self.state = 'wait'
-        Thread(target=self._am).start()
-
-    def apply_move(self, move):
-        self.moves = self.moves[:self.move_step]
-        self.moves.append(move)
-        self.gret()
+    def _am(self, d=False):
+        if d:
+            self.gret()
         if not self.board.get_pms():
             self.state = 'play'
             return
-        if ((self.ai_chn and ' w ' in self.board.fen) or
-            (self.ai_int and ' b ' in self.board.fen)):
-            self.auto_move()
+        self.state = 'wait'
+        try:
+            move = self.board.get_best_move(think_time=self.ai_think_time)
+        except RuntimeError:
+            return
+        self.apply_move(move)
+
+    def auto_move(self, do_gret=False):
+        Thread(target=self._am,args=(do_gret,)).start()
+
+    def apply_move(self, move):
+        if len(self.moves) != self.move_step:
+            self.moves = self.moves[:self.move_step]
+        self.moves.append(move)
+        if ((self.ai_chn and ' b ' in self.board.fen) or
+            (self.ai_int and ' w ' in self.board.fen)):
+            self.auto_move(True)
         else:
-            self.state = 'play'
+            self.thread_gret()
 
     # 走子模式下，棋盘上点击的操作，控制高亮路径和选中的棋子
     def handle_board_event(self,beach_p):
         # 是一步走子
         if beach_p in self.highlight_paths:
             # 添加动画 并且更新 beach 状态
-            move = beach.beach2fsf(self.last_choice_piece[0]) + beach.beach2fsf(beach_p)
+            fp = self.last_choice_piece[0]
+            move = beach.beach2fsf(fp) + beach.beach2fsf(beach_p)
+            self.reset_special_pieces_show()
+            tp = beach_p
+            # 如果走子是升变
+            if self.board[fp] == 13 and tp//9 == 8:
+                self.state = 'promotion'
+                self.promotion_move = move
+
+                self.piece_animations.append((tp, tp, -1, 1, 14))
+                self.piece_animations.append((tp - 9, tp - 9, -1, 1, 14))
+                self.piece_animations.append((tp - 18, tp - 18, -1, 1, 14))
+                self.piece_animations.append((tp - 27, tp - 27, -1, 1, 14))
+                self.piece_animations.append((tp, tp, -1, 1, 11))
+                self.piece_animations.append((tp - 9, tp - 9, -1, 1, 10))
+                self.piece_animations.append((tp - 18, tp - 18, -1, 1, 9))
+                self.piece_animations.append((tp - 27, tp - 27, -1, 1, 8))
+                return
+            # 中国象棋的升变
+            # elif self.board[fp] == 7 and tp//9 == 0:
+            #     move += 'j'
             self.apply_move(move)
             return
         # 如果没选子或选了一样的，直接重置
@@ -145,15 +202,23 @@ class Game:
             self.reset_special_pieces_show()
             p, typ = beach_p, self.board[beach_p]
             if typ >= 0:
-                self.piece_animations.append((p + 0.15, p, 0, 3, typ))
-                self.piece_animations.append((p, p + 0.15, -3, 3, typ))
+                if self.board_is_flipped:
+                    self.piece_animations.append((p - 0.15, p, 0, 3, typ))
+                    self.piece_animations.append((p, p - 0.15, -3, 3, typ))
+                    self.piece_animations.append((p - 0.15, p, -6, 3, typ))
+                    self.piece_animations.append((p, p - 0.15, -9, 3, typ))
+                else:
+                    self.piece_animations.append((p + 0.15, p, 0, 3, typ))
+                    self.piece_animations.append((p, p + 0.15, -3, 3, typ))
+                    self.piece_animations.append((p + 0.15, p, -6, 3, typ))
+                    self.piece_animations.append((p, p + 0.15, -9, 3, typ))
             return
         self.last_choice_piece = (beach_p, self.board[beach_p])
 
-    def undo(self):
+    def undo(self, steps=1):
         if self.move_step > 0:
             self.reset_special_pieces_show()
-            self.move_step -= 1
+            self.move_step -= steps
             self.board.moves_reset(self.moves[:self.move_step])
             move = self.moves[self.move_step]
             fp = beach.fsf2beach(move[:2])
@@ -163,8 +228,10 @@ class Game:
                 self.piece_animations.append((tp, tp, 0, 10, eat_typ))
             piece_typ = self.board[fp]
             if move == 'd9b9' and piece_typ == 12:
+                self.piece_animations.append((2, 2, 0, 10, 8))
                 self.piece_animations.append((2, 0, -10, 10, 8))
             if move == 'd9f9' and piece_typ == 12:
+                self.piece_animations.append((4, 4, 0, 10, 8))
                 self.piece_animations.append((4, 8, -10, 10, 8))
             if piece_typ >= 0:
                 self.piece_animations.append((tp, fp, 0, 10, piece_typ))
@@ -180,13 +247,50 @@ class Game:
                 self.piece_animations.append((tp, tp, 0, 10, eat_typ))
             piece_typ = self.board[fp]
             if move == 'd9b9' and piece_typ == 12:
+                self.piece_animations.append((0, 0, 0, 10, 8))
                 self.piece_animations.append((0, 2, -10, 10, 8))
             if move == 'd9f9' and piece_typ == 12:
+                self.piece_animations.append((8, 8, 0, 10, 8))
                 self.piece_animations.append((8, 4, -10, 10, 8))
             if piece_typ >= 0:
                 self.piece_animations.append((fp, tp, 0, 10, piece_typ))
             self.move_step += 1
             self.board.moves_reset(self.moves[:self.move_step])
+
+    def reset(self, fen = None):
+        if fen is None:
+            self.board.reset()
+        else:
+            self.board.reset(fen)
+
+        self.steady_pieces.clear()
+        self.highlight_paths.clear()
+        self.piece_animations.clear()
+        self.last_choice_piece = (-1, -1)
+        self.UIs.clear()
+        self.pressed_button = [-1, 0]
+
+        self.state = 'play'
+        self.moves = []
+        self.move_step = 0
+        self.ai_chn = False
+        self.ai_int = False
+
+    def _tu(self,s):
+        self.state = 'waits'
+        self.undo(s)
+        self.state = 'play'
+
+    def thread_undo(self,s=1):
+        Thread(target=self._tu,args=(s,)).start()
+
+    def _tg(self):
+        self.state = 'waits'
+        self.gret()
+        self.state = 'play'
+
+    def thread_gret(self):
+        Thread(target=self._tg).start()
 
     def quit(self):
         self.board.suicide()
